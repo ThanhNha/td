0f9466c8157ed22f79128b5e4e69be766717acfe
ThanhNha <72262090+ThanhNha@users.noreply.github.com>
2022-06-13 08:49:35 +0700

    add overlay

--- assets/script/header.js
+++ assets/script/header.js
@@ -1,8 +1,8 @@
 const hamburger = $("#hamburger-menu");
 const body = $("body");
+const overlay = $("#overlay");
 
 hamburger.click(() => {
-  hamburger.toggleClass("active");
   body.toggleClass("open-menu");
 });
 
@@ -11,4 +11,12 @@ let sub = $(".sub-menu-wrapper");
 has_sub.click((e) => {
   e.preventDefault();
   sub.toggleClass("open-sub");
+});
+overlay.click(() => {
+  body.removeClass("open-menu");
+});
+$(document).on("click", function (e) {
+  if ($(e.target).closest("#sidebar").length === 0) {
+    $("#sidebar .sub-menu-wrapper").removeClass("open-sub");
+  }
 });
